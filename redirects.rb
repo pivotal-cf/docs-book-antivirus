f9524f10e32937134f032dbbc1027b08b69666e4
Samia Nneji <snneji@pivotal.io>
2018-04-06 16:48:33 +0100

    Remove ClamAV 1.2 from config.yml [#156479650]

--- redirects.rb
+++ redirects.rb
@@ -1,2 +1,3 @@
 # default product redirect
 r301 %r{/addon-antivirus/(?![\d-]+)(.*)}, "/addon-antivirus/1-4/$1"
+r301 %r{/addon-antivirus/1-2/(.*)}, 'http://docs.pivotal.io/archives/addon-antivirus-1.2.pdf'
