b00f67e4f1843ba486962f65e044ba3903ffebc8
animatedmax <mhufnagel@pivotal.io>
2018-08-09 15:55:53 -0700

    Redirect http traffic to https on production

--- redirects.rb
+++ redirects.rb
@@ -1,3 +1,8 @@
+# Redirect all production http traffic to https
+r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
+  rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
+}
+
 # default product redirect
 r301 %r{/addon-antivirus/(?![\d-]+)(.*)}, "/addon-antivirus/1-4/$1"
 r301 %r{/addon-antivirus/1-2/(.*)}, 'http://docs.pivotal.io/archives/addon-antivirus-1.2.pdf'
