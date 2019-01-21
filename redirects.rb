a6e45aa9ad55f5ff9a048a29c1e760aae1821aa0
Chris Wong <cwong@pivotal.io>
2019-01-21 17:40:27 -0500

    redirects for v1.3 pdf [#163159591]

--- redirects.rb
+++ redirects.rb
@@ -5,4 +5,5 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 
 # default product redirect
 r301 %r{/addon-antivirus/(?![\d-]+)(.*)}, "/addon-antivirus/1-4/$1"
+r301 %r{/addon-antivirus/1-3/(.*)}, 'http://docs.pivotal.io/archives/addon-antivirus-1.3.pdf'
 r301 %r{/addon-antivirus/1-2/(.*)}, 'http://docs.pivotal.io/archives/addon-antivirus-1.2.pdf'
