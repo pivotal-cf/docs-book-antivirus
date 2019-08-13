d6bff94531467a62f48d671f129dc151b085b4c1
Chris Wong <cwong@pivotal.io>
2019-08-13 16:45:59 -0400

    2-0 prod avoidance [#167882236]

--- redirects.rb
+++ redirects.rb
@@ -3,7 +3,10 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
+# temporary 2-0 avoidance
+r301 %r{/addon-antivirus/2-0/(.*)}, "/addon-antivirus/1-4/$1"
+
 # default product redirect
-r301 %r{/addon-antivirus/(?![\d-]+)(.*)}, "/addon-antivirus/2-0/$1"
+r301 %r{/addon-antivirus/(?![\d-]+)(.*)}, "/addon-antivirus/1-4/$1"
 r301 %r{/addon-antivirus/1-3/(.*)}, 'http://docs.pivotal.io/archives/addon-antivirus-1.3.pdf'
 r301 %r{/addon-antivirus/1-2/(.*)}, 'http://docs.pivotal.io/archives/addon-antivirus-1.2.pdf'
