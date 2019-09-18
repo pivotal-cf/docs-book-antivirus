03efd486465807cddd7b3af97eb42a31417d7532
Chris Wong <cwong@pivotal.io>
2019-09-18 10:59:35 -0400

    Update default to 2-0 [#167622123]

--- redirects.rb
+++ redirects.rb
@@ -7,6 +7,6 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 # r302 %r{docs.pivotal.io/addon-antivirus/2-0/(.*)}, "docs.pivotal.io/addon-antivirus/1-4/$1"
 
 # default product redirect
-r301 %r{/addon-antivirus/(?![\d-]+)(.*)}, "/addon-antivirus/1-4/$1"
+r301 %r{/addon-antivirus/(?![\d-]+)(.*)}, "/addon-antivirus/2-0/$1"
 r301 %r{/addon-antivirus/1-3/(.*)}, 'http://docs.pivotal.io/archives/addon-antivirus-1.3.pdf'
 r301 %r{/addon-antivirus/1-2/(.*)}, 'http://docs.pivotal.io/archives/addon-antivirus-1.2.pdf'
