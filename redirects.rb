36c8d048deacc46c8515135bccec15b58080831d
Chris Wong <cwong@pivotal.io>
2019-09-18 10:58:54 -0400

    Remove 2.0 avoidance [#167622123]

--- redirects.rb
+++ redirects.rb
@@ -4,7 +4,7 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 }
 
 # temporary 2-0 avoidance
-r302 %r{docs.pivotal.io/addon-antivirus/2-0/(.*)}, "docs.pivotal.io/addon-antivirus/1-4/$1"
+# r302 %r{docs.pivotal.io/addon-antivirus/2-0/(.*)}, "docs.pivotal.io/addon-antivirus/1-4/$1"
 
 # default product redirect
 r301 %r{/addon-antivirus/(?![\d-]+)(.*)}, "/addon-antivirus/1-4/$1"
