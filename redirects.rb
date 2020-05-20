2cb41de0e074c06b63a536d7f0e1a16f41e6a455
RichardJJG <45788327+RichardJJG@users.noreply.github.com>
2020-05-20 10:32:04 +0100

    Moved the PDF redirects [#172788008]

--- redirects.rb
+++ redirects.rb
@@ -5,7 +5,3 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 
 # temporary 2-0 avoidance
 # r302 %r{docs.pivotal.io/addon-antivirus/2-0/(.*)}, "docs.pivotal.io/addon-antivirus/1-4/$1"
-
-# default product redirect
-r301 %r{/addon-antivirus/1-3/(.*)}, 'http://docs.pivotal.io/archives/addon-antivirus-1.3.pdf'
-r301 %r{/addon-antivirus/1-2/(.*)}, 'http://docs.pivotal.io/archives/addon-antivirus-1.2.pdf'
