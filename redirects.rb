c02b844cadbf1fe5818d7b19f0614c60600f5a3a
animatedmax <mhufnagel@pivotal.io>
2022-03-02 13:36:11 -0800

    [#180999714] redirect anti-virus from docs.pivotal.io to docs.vmware.com

--- redirects.rb
+++ redirects.rb
@@ -3,5 +3,10 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-# temporary 2-0 avoidance
-# r302 %r{docs.pivotal.io/addon-antivirus/2-0/(.*)}, "docs.pivotal.io/addon-antivirus/1-4/$1"
+# Redirect for Antivirus
+r302 %r{/addon-antivirus/(?![\d-]+)(.*)}, "https://docs.vmware.com/en/Anti-Virus-for-VMware-Tanzu/2.3/anti-virus/GUID-index.html"
+r302 %r{/addon-antivirus/2-3/(.)}, "https://docs.vmware.com/en/Anti-Virus-for-VMware-Tanzu/2.3/anti-virus/GUID-index.html"
+r302 %r{/addon-antivirus/2-2/(.)}, "https://docs.vmware.com/en/Anti-Virus-for-VMware-Tanzu/2.2/anti-virus/GUID-index.html"
+r302 %r{/addon-antivirus/2-1/(.)}, "https://docs.vmware.com/en/Anti-Virus-for-VMware-Tanzu/2.1/anti-virus/GUID-index.html"
+r302 %r{/addon-antivirus/2-0/(.)}, "https://docs.vmware.com/en/Anti-Virus-for-VMware-Tanzu/2.0/anti-virus/GUID-index.html"
+r302 %r{/addon-antivirus/1-4/(.)}, "https://docs.vmware.com/en/Anti-Virus-for-VMware-Tanzu/1-4/anti-virus/GUID-index.html"
